cc1f0b49f907e4370ad9a56108cf9da324a1adbc
AlexanderA <alexander.alferovich@gmail.com>
2023-01-11 13:17:27 +0400

    Final commit

--- less1_Task6_C#_homework_Alferovich.cs
+++ less1_Task6_C#_homework_Alferovich.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Пожалуйста, введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.WriteLine("Это чётное число.");
+}
+else
+{
+    Console.WriteLine("Это нечётное число.");
+}
